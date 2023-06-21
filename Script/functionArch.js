b78f910f6656cfbbee393942c95796a4b4156ee9
Tabarin01 <119614157+Tabarin01@users.noreply.github.com>
2023-06-21 15:50:14 +0200

    ended

--- Script/functionArch.js
+++ Script/functionArch.js
@@ -1,31 +1,32 @@
+
 //Link partners
 $(document).ready(function () {
-  $(".p1").click(async function () {
+  $(".p1").on("click touchstart", function () {
     window.open("https://www.cittametropolitana.bo.it/portale/", "_blank");
   });
 
-  $(".p2").click(async function () {
+  $(".p2").on("click touchstart", function () {
     window.open("https://www.foiatonda.it/", "_blank");
   });
-  $(".p3").click(async function () {
+  $(".p3").on("click touchstart",function () {
     window.open("https://www.emilbanca.it/", "_blank");
   });
-  $(".p4").click(async function () {
+  $(".p4").on("click touchstart",function () {
     window.open("https://www.appenninoslow.it/", "_blank");
   });
-  $(".p5").click(async function () {
+  $(".p5").on("click touchstart",function () {
     window.open("https://www.regione.emilia-romagna.it/", "_blank");
   });
-  $(".p6").click(async function () {
+  $(".p6").on("click touchstart",function () {
     window.open("https://www.bolognamontana.it/", "_blank");
   });
-  $(".p7").click(async function () {
+  $(".p7").on("click touchstart",function () {
     window.open("https://www.lineagotica.eu/", "_blank");
   });
-  $(".p8").click(async function () {
+  $(".p8").on("click touchstart",function () {
     window.open("https://www.comune.monzuno.bo.it/home", "_blank");
   });
-  $(".p10").click(async function () {
+  $(".p10").on("click touchstart",function () {
     window.open("https://www.savenasettasambro.com/", "_blank");
   });
 });
