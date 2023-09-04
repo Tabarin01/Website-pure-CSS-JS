e418f8144ce4c2ef9abcc19afed9a7d4135b7857
Tabarin01 <119614157+Tabarin01@users.noreply.github.com>
2023-09-04 12:13:07 +0200

    rezise di tutto

--- Script/functionArch.js
+++ Script/functionArch.js
@@ -4,12 +4,12 @@
 window.addEventListener("load", function () {
   const container = document.querySelector(".dcontainer");
   container.style.backgroundImage =
-    "linear-gradient(to right, rgb(0 0 0 / 80%), rgb(0 0 0 / 0%)), url('../images/imgBackgrounds/A-D.png')";
+    "linear-gradient(to right, rgb(0 0 0 / 80%), rgb(0 0 0 / 0%)), url('../images/imgBackgrounds/A-D.jpg')";
 });
 window.addEventListener("load", function () {
   const container = document.querySelector(".second");
   container.style.backgroundImage =
-    "linear-gradient(to right, rgb(0 0 0 / 80%), rgb(0 0 0 / 0%)), url('../images/imgBackgrounds/A-D.png')";
+    "linear-gradient(to right, rgb(0 0 0 / 80%), rgb(0 0 0 / 0%)), url('../images/imgBackgrounds/A-D.jpg')";
 });
 
 /*********************************************************
